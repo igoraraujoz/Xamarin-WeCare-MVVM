014e623553f8686fb8be25a9310e27b34517ddc8
Igor <igor_luan13@hotmail.com>
2019-07-21 13:00:36 -0300

    FFLoadImage e Timeline

--- WeCare/WeCare.Android/MainActivity.cs
+++ WeCare/WeCare.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace WeCare.Droid
 {
@@ -18,8 +19,16 @@ namespace WeCare.Droid
             ToolbarResource = Resource.Layout.Toolbar;
             Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
             base.OnCreate(savedInstanceState);
+            CachedImageRenderer.Init(false);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
---
+++ WeCare/WeCare/Controls/CustomListView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace WeCare.Controls
+{
+    public class CustomListView : ListView
+    {
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(CustomListView), null);        
+
+        public ICommand Command
+        {
+            get { return (ICommand)this.GetValue(CommandProperty); }
+            set { this.SetValue(CommandProperty, value); }
+        }
+
+        public CustomListView()
+        {
+            this.ItemTapped += OnItemTapped;            
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null)
+            {
+                Command?.Execute(e.Item);
+                SelectedItem = null;
+            }
+        }
+    }
+}
